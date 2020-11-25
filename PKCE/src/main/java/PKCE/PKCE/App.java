85028b8f32c1f3ae534a926118e43c7ae28673fb
sothon <huon.sothon@gmail.com>
2020-11-25 18:08:25 +0700

    initial commit

---
+++ PKCE/src/main/java/PKCE/PKCE/App.java
@@ -0,0 +1,13 @@
+package PKCE.PKCE;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ photos/src/test/java/com/appsdeveloperblog/api/ResourceServer/ResourceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appsdeveloperblog.api.ResourceServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
